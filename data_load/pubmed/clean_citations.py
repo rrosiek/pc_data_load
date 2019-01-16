58081fe911d8ef8f6fcdc317ba5f69375dcae2a7
Robin <robin@QB-TP3-1123.local>
2019-01-16 17:20:50 +0530

    Clean pubmed citations

--- data_load/pubmed/clean_citations.py
+++ data_load/pubmed/clean_citations.py
@@ -4,11 +4,14 @@ from data_load.pubmed2019.pubmed_data_extractor import PubmedDataExtractor
 from data_load.pubmed2019.pubmed_data_mapper import PubmedDataMapper
 from data_load.base.load_config import LoadConfig
 from data_load.base.data_source_xml import XMLDataSource
+from data_load.base.utils.data_loader_utils import DataLoaderUtils
 
 from data_load.pubmed.ftp_manager import FTPManager
 import data_load.pubmed.file_manager as file_manager
 
 import psutil
+
+import sys
 
 ID_PUBMED_2019 = 'PUBMED_2019'
 
@@ -22,9 +25,9 @@ TYPE = 'article'
 
 class CleanCitations(object):
 
-
     def __init__(self):
-        self.ids = {}
+        self.updated_docs = {}
+        self.original_docs = {}
 
         self.server = SERVER
         self.index = INDEX
@@ -34,8 +37,77 @@ class CleanCitations(object):
         self.server_password = ''
 
         self.load_config = self.get_load_config(clean_citations_directory)
+        self.data_loader_utils = DataLoaderUtils(self.load_config.server, self.load_config.index, self.load_config.type, self.load_config.server_username, self.load_config.server_password)
 
     def run(self):
+        self.get_updated_docs()
+        self.get_original_docs()
+
+        print 'Updated docs:', len(self.updated_docs)
+        print 'Original docs:', len(self.original_docs)
+        input = raw_input('Continue?')
+        if input.lower() in ['n', 'no', '0']:
+            sys.exit(1)
+        self.update_doc()
+
+    def update_docs(self):
+        for _id in self.updated_docs:
+            if _id in self.original_docs:
+                original_doc = self.original_docs[_id]
+                original_citations = self.load_config.data_mapper.get_citations(original_doc)
+                self.update_doc(_id, original_citations)
+
+    def update_doc(self, _id, original_citations):
+        print 'Updating doc', _id, len(original_citations), 'citations'
+        # Get the existing update history
+        update_history = []
+    
+        # Add the original citations list if not present
+        if len(update_history) == 0:
+            update_history.append({
+                "original_citations": original_citations
+            })
+
+        doc = {
+            "update_history": update_history
+        }
+        
+        doc = {
+            'doc': doc
+        }
+
+        self.data_loader_utils.update_doc(_id, doc)          
+
+
+    def get_original_docs(self):
+        load_config = self.get_load_config(baseline_directory)
+        ftp_manager = FTPManager(load_config)
+
+        baseline_file_urls = ftp_manager.get_baseline_file_urls()
+        ftp_manager.download_missing_files(file_urls=baseline_file_urls, no_of_files=10)
+        baseline_files = file_manager.get_baseline_files(load_config, baseline_file_urls)
+
+        print 'Baseline files:', len(baseline_files)
+
+        for baseline_file in baseline_files:
+            print "Processing file:", baseline_file
+            xml_data_source = XMLDataSource(baseline_file, 2)
+            xml_data_source.process_rows(self.process_baseline_row)
+
+
+    def process_baseline_row(self, row, current_index):
+        _id = self.extract_id(self.load_config.data_source_name, row, current_index)
+        if _id is not None and _id in self.updated_docs:
+            doc = self.extract_data(_id, self.load_config.data_source_name, row)
+            if doc is not None and len(doc) > 0:
+                self.original_docs[_id] = doc
+
+            if len(self.original_docs) % 100 == 0:
+                print 'Original docs', len(self.original_docs)
+
+        return True
+
+    def get_updated_docs(self)
         load_config = self.get_load_config(updates_directory)
         ftp_manager = FTPManager(load_config)
 
@@ -49,30 +121,21 @@ class CleanCitations(object):
         # files_to_process = file_manager.get_new_update_files(load_config, update_file_urls, 2)
         print files_to_process
 
-        self.get_docs()
+        for update_file in files_to_process:
+            xml_data_source = XMLDataSource(update_file, 2)
+            xml_data_source.process_rows(self.process_row)
 
-        # for update_file in files_to_process:
-        #     xml_data_source = XMLDataSource(update_file, 2)
-        #     xml_data_source.process_rows(self.process_row)
-
-        # print 'total ids:', self.ids
-
-    def get_docs(self):
-        load_config = self.get_load_config(baseline_directory)
-        ftp_manager = FTPManager(load_config)
-
-        baseline_file_urls = ftp_manager.get_baseline_file_urls()
-        baseline_files = file_manager.get_baseline_files(load_config, baseline_file_urls)
-
-        print 'Baseline files:', len(baseline_files)
+        print 'Total updated ids:', len(self.updated_docs)
 
     def process_row(self, row, current_index):
         _id = self.extract_id(self.load_config.data_source_name, row, current_index)
-        if _id is not None and _id not in self.ids:
-            self.ids[_id] = 0
+        if _id is not None and _id not in self.updated_docs:
+            doc = self.extract_data(_id, self.load_config.data_source_name, row)
+            if doc is not None and len(doc) > 0:
+                self.updated_docs[_id] = doc
 
-            if len(self.ids) % 1000 == 0:
-                print len(self.ids)
+            if len(self.updated_docs) % 1000 == 0:
+                print 'Updated docs', len(self.updated_docs)
 
         return True
 
@@ -113,6 +176,11 @@ class CleanCitations(object):
         self.load_config.log(LOG_LEVEL_WARNING, 'Error: no data extractor configured')
         return None
 
+    def extract_data(self, _id, name, row):
+        if self.load_config.data_extractor is not None:
+            return self.load_config.data_extractor.extract_data(_id, name, row)
+
+        return row
 
 clean_citations = CleanCitations()
 clean_citations.run()
