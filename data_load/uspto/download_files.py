0ceda20fb965fa2b0c8cafd451300b47f7e35c54
Robin <robin@QB-TP3-1123.local>
2019-01-24 15:10:33 +0530

    USPTO create mapping

--- data_load/uspto/download_files.py
+++ data_load/uspto/download_files.py
@@ -8,8 +8,26 @@ import zipfile
 import os
 import urllib
 
+USPTO_DOWNLOADED_FILES = 'USPTO_DOWNLOADED_FILES.json'
+
+source_files_directory = '/data/data_loading/source-files/uspto/'
+
+
+def load_files_per_year():
+    file_utils.make_directory(source_files_directory)
+    return file_utils.load_file(source_files_directory, 'files_per_year.json')
+
+def save_files_per_year(files_per_year):
+    file_utils.make_directory(source_files_directory)
+    file_utils.save_file(source_files_directory, 'files_per_year.json', files_per_year)
+
 def get_available_files_to_download(year=None):
-    files_per_year = get_data_source_links.run()
+    files_per_year = load_files_per_year()
+
+    if files_per_year == None or len(files_per_year) == 0:
+        files_per_year = get_data_source_links.run()
+        save_files_per_year(files_per_year)
+
     print len(files_per_year)
     if year is not None:
         urls = files_per_year[year]
@@ -22,13 +40,24 @@ def get_available_files_to_download(year=None):
             urls.extend(files_per_year[year])
 
     print urls
-    return urls   
+    return urls
+
+
+def get_downloaded_files(other_files_directory):
+    downloaded_files = file_utils.load_file(other_files_directory, USPTO_DOWNLOADED_FILES)
+    if len(downloaded_files) == 0:
+        downloaded_files = []
+    return downloaded_files
+
+def set_downloaded_files(other_files_directory, downloaded_files):
+    file_utils.save_file(other_files_directory, USPTO_DOWNLOADED_FILES, downloaded_files)
 
 
 def download_files(year=None):
     files_to_download = get_available_files_to_download(year=year)
-    source_files_directory = '/data/data_loading/source-files/uspto/'
     file_utils.make_directory(source_files_directory)
+
+    downloaded_update_file_urls = get_downloaded_files(source_files_directory)
     downloaded_update_file_paths = []
 
     print 'Downloading', len(files_to_download), 'files...'
@@ -52,6 +81,7 @@ def download_files(year=None):
             with zipfile.ZipFile(update_file_path, 'r') as zip_ref:
                 zip_ref.extractall(source_files_directory)
 
+            downloaded_update_file_urls.append(update_file_url)
             downloaded_update_file_paths.append(xml_file_path)
 
         except Exception as e:
@@ -67,6 +97,7 @@ def download_files(year=None):
         os.remove(update_file_path)
 
     # Save the downloaded files list
+    set_downloaded_files(source_files_directory, downloaded_update_file_urls)
 
     return downloaded_update_file_paths
 
