2ebbaa10797966f4327f1c3319c0ace82dc16d05
Robin <robin@QB-TP3-1123.local>
2019-01-25 15:34:36 +0530

    irdb data check

--- data_load/irdb/fetch_ids.py
+++ data_load/irdb/fetch_ids.py
@@ -1,5 +1,5 @@
 from data_load.base.utils.export_doc_ids import export_doc_ids
-
+from data_load.base.data_source_csv import CSVDataSource
 
 
 SERVER = 'http://localhost:9200'
@@ -12,11 +12,69 @@ def run():
     
     doc_ids = doc_ids.keys()
     doc_ids.sort()
-    
+
     print len(doc_ids)
     print doc_ids[-4]
     print doc_ids[-3]
     print doc_ids[-2]
     print doc_ids[-1]
 
-run()
+irdb_ids = {}
+
+
+def process_data():
+
+    appls_at = '/data/data_loading/old-records/irdb_2018_06/source_files/h/APPLS_AT.csv'
+    appls_mv = '/data/data_loading/old-records/irdb_2018_06/source_files/h/APPLS_MV.csv'
+
+    print 'processing', appls_at
+    csv_data_source = CSVDataSource(appls_at)
+    csv_data_source.process_rows()
+
+    print 'processing', appls_mv
+    csv_data_source1 = CSVDataSource(appls_mv)
+    csv_data_source1.process_rows()
+
+
+    print len(irdb_ids), 'total ids'
+    sorted_ids = irdb_ids.keys()
+    sorted_ids.sort()
+
+    print sorted_ids[-3]
+    print sorted_ids[-2]
+    print sorted_ids[-1]
+
+    id_to_search = '9726225'
+    if id_to_search in irdb_ids:
+        print id_to_search, irdb_ids[id_to_search]
+    else:
+        print id_to_search, 'not found'
+
+    id_to_search = '9554780'
+    if id_to_search in irdb_ids:
+        print id_to_search, irdb_ids[id_to_search]
+    else:
+        print id_to_search, 'not found'
+
+
+def process_row(row, current_index):
+    appl_id = None
+    if 'APPL_ID' in row:
+        appl_id = str(row['APPL_ID'])
+
+    grant_num = ""
+    if 'GRANT_NUM' in row:
+        grant_num = row['GRANT_NUM']
+
+    if appl_id is not None:
+        if appl_id not in irdb_ids:
+            irdb_ids[appl_id] = []
+
+        irdb_ids[appl_id].append(grant_num)
+
+    if current_index % 1000 == 0:
+        print 'Processed', current_index, 'ids', appl_id
+
+process_data()
+
+
