da5ed31db3048d4ad2e0fb32efbfaa9aab850981
Robin <robin@QB-TP3-1123.local>
2019-01-25 14:22:05 +0530

    irdb scripts

--- data_load/irdb/fetch_ids.py
+++ data_load/irdb/fetch_ids.py
@@ -9,8 +9,10 @@ TYPE = 'grant'
 
 def run():
     doc_ids = export_doc_ids(server=SERVER, src_index=INDEX, src_type=TYPE)
+    
+    doc_ids = doc_ids.keys()
     doc_ids.sort()
-
+    
     print len(doc_ids)
     print doc_ids[-4]
     print doc_ids[-3]
